77b814bb40812f938aebb3cd50bcc2195cb9545e
Klaus <Klaus1993@live.com>
2019-11-04 20:01:08 +0000

    add english

--- resources/js/components/RecommendForm.vue
+++ resources/js/components/RecommendForm.vue
@@ -8,7 +8,7 @@
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
             {{ $t('fullname') }}
         </label>
-        <input id="grid-first-name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Jane Doe">
+        <input id="grid-first-name" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Jane Doe">
       </div>
       <div class="w-full md:w-1/2 px-3">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
