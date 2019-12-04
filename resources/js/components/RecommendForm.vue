db50eeb43d1436b043cb6975c8fcb2dd71d4f6ef
Klaus <Klaus1993@live.com>
2019-12-04 17:39:45 +0000

    add more fields for rec form

--- resources/js/components/RecommendForm.vue
+++ resources/js/components/RecommendForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="w-full max-w-lg px-4">
+  <form class="w-full max-w-lg px-4 py-6">
       <h1 class="text-3xl mb-6">
           {{ $t('recommend_a_host') }}
       </h1>
@@ -23,9 +23,30 @@
         <input id="grid-phone" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="tel" placeholder="+000 000 000 000">
       </div>
     </div>
+    <hr class="pb-1 border-t">
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full px-3 mb-6 md:mb-0">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            {{ $t('his_fullname') }}
+        </label>
+        <input id="grid-his-first-name" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Jane Doe">
+      </div>
+      <div class="w-full md:w-1/2 px-3">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
+            {{ $t('his_email') }}
+        </label>
+        <input id="grid-his-email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" placeholder="jane@doe.com">
+      </div>
+      <div class="w-full md:w-1/2 px-3">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-his-phone">
+            {{ $t('his_phone') }}
+        </label>
+        <input id="grid-phone" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="tel" placeholder="+000 000 000 000">
+      </div>
+    </div>
 
     <div class="flex items-end mt-6">
-      <tw-button class="w-full">
+      <tw-button class="w-full my-4">
         Enviar
       </tw-button>
     </div>
